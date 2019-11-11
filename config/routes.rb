499a9385d20b9e768b87314db6689db4e80258dc
moritakentarou <1993mxxakmkxxm@gmail.com>
2019-11-11 20:42:28 +0900

    グループ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
+=======
+  devise_for :users
+>>>>>>> Stashed changes
   root 'messages#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update]
