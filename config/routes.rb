0454cb3f9a0562f800f2d9b54bb422e431c93f50
moritakentarou <1993mxxakmkxxm@gmail.com>
2019-12-26 17:39:51 +0900

    インクリ完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
+# Rails.application.routes.draw do
+#   devise_for :users
+#   # devise_for :users
+#   root 'groups#index'
+#   root 'users#index'
+#   resources :users, only: [:index, :edit, :update]
+#   resources :groups, only: [:index, :new, :create, :edit, :update]do
+#   resources :messages, only: [:index, :create,:edit]
+#   get '/edit/:id', to: 'edit#user', as: :edit_user
+#   end
+# end
 Rails.application.routes.draw do
   devise_for :users
-  # devise_for :users
   root 'groups#index'
+  # :indexを追記
   resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]do
-  resources :messages, only: [:index, :create,:edit]
-  get '/edit/:id', to: 'edit#user', as: :edit_user
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 end
